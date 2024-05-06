32a0c3e6a36fd13681c83ca5fd1d47b5ff26d5da
softelectrical-tech <info@softelectrical.com>
2024-05-06 13:34:56 +0100

    Created profile_button function

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,9 @@ print("Hello World")
 
 #my second print statement
 print("My second Hello World")
+
+def profile_button():
+    print("I will update user profile when clicked")
+
+
+profile_button()
